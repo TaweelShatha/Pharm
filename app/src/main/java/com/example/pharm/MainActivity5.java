046b6cb429dbc32e94b00a9ef1e29d0587ebed58
omar <itsomar278@gmail.com>
2023-07-09 18:54:25 +0300

    some additional edits upload user pics in progress

--- app/src/main/java/com/example/pharm/MainActivity5.java
+++ app/src/main/java/com/example/pharm/MainActivity5.java
@@ -1,53 +1,29 @@
 package com.example.pharm;
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.ContentValues;
-import android.content.DialogInterface;
+
+import android.content.ContentResolver;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.util.HashMap;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity5 extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 1;
     private FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
     FirebaseDatabase firebaseDatabase;
@@ -57,28 +33,78 @@ public class MainActivity5 extends AppCompatActivity {
     String uid;
     ImageView set;
     TextView profilepic, editname, editpassword;
-    ProgressDialog pd;
-    private static final int CAMERA_REQUEST = 100;
-    private static final int STORAGE_REQUEST = 200;
-    private static final int IMAGEPICK_GALLERY_REQUEST = 300;
-    private static final int IMAGE_PICKCAMERA_REQUEST = 400;
     String cameraPermission[];
     String storagePermission[];
     Uri imageuri;
     String profileOrCoverPhoto;
+    FirebaseUser user;
+    private Uri mImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        user = FirebaseAuth.getInstance().getCurrentUser();
         setContentView(R.layout.activity_main5);
         editpassword = findViewById(R.id.changepassword);
+        profilepic = findViewById(R.id.profilepic);
+        set = findViewById(R.id.setting_profile_image);
+        storageReference = FirebaseStorage.getInstance().getReference("UsersPics");
+        databaseReference = FirebaseDatabase.getInstance().getReference("UsersPics");
+
+
+
         editpassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             }
         });
+        profilepic.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                openFileChooser();
+            }
+        });
 
     }
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null)
+        {
+            mImageUri = data.getData();
+          Picasso.get().load(mImageUri).into(set);
+          UploadImage();
+
+        }
+    }
+
+    public  void UploadImage()
+    {
+        if (mImageUri!=null)
+        {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            StorageReference fileRef = storageReference.child(user.getEmail()
+                    +"."+getFileExtension(mImageUri));
+
+        }
+        else
+        {
+            Toast.makeText(this,"NO Image Selected" ,Toast.LENGTH_SHORT);
+        }
+    }
+    public String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
 
 }
